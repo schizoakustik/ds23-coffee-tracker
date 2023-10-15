91e5d04bc8c00e25891a341b31833824382d55fc
schizoakustik <schizoakustik@schizoakustik.se>
2023-10-15 15:14:24 +0200

    some cleaning

--- static/script.js
+++ static/script.js
@@ -10,11 +10,11 @@ body.style.background = colors[data.currentLocation];
 for (const [key, value] of Object.entries(colors)) {
     var div = document.createElement('div')
     div.id = key
+    div.style.background = value
     div.style.width = '5em'
     div.style.height = '5em'
     div.style.border = '1px solid black'
     div.style.borderRadius = '5px'
-    div.style.background = value
     boxes.style.opacity = .6
     div.style.cursor = 'pointer'
     div.addEventListener('click', (e) => {
@@ -36,8 +36,4 @@ function set_color(loc) {
             body: JSON.stringify({currentLocation: newLocation})
         }
         ).then()
-    }
-    
-function setBackground() {
-
-}
+}    
